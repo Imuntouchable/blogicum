832daff66430170025b6575d3a082e58e1323c7a
Александр <sasatoguzov@gmail.com>
2023-11-18 21:58:01 +0300

    Remove all files

--- blogicum/blog/mixins.py
+++
@@ -1,8 +0,0 @@
-from blog.models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-class PostMixin(LoginRequiredMixin):
-    model = Post
-    pk_url_kwarg = 'post_id'
-    template_name = 'blog/create.html'
